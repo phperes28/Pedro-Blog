77d9b18f427502db3703e2cbceb788453686c483
Sckhar <pedrosilva28@gmail.com>
2021-08-11 17:53:07 -0300

    Implement new flask-mail config

--- main.py
+++ main.py
@@ -20,13 +20,11 @@ app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 # os.environ.get("SECRET_KEY")
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
- #TURN INTO ENVIRON
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = os.environ.get("MY_EMAIL")
 app.config['MAIL_PASSWORD'] = os.environ.get("PASS")
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
-
 
 
 mail = Mail(app)
